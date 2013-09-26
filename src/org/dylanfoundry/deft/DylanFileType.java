87d658b43201602da6a8da3bc5a862c2f8377c10
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-09-26 16:17:43 +0700

    Initial commit.
    This recognizes Dylan files.

---
+++ src/org/dylanfoundry/deft/DylanFileType.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013, Bruce Mitchener, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dylanfoundry.deft;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class DylanFileType extends LanguageFileType {
+  public static final LanguageFileType INSTANCE = new DylanFileType();
+  @NonNls
+  public static final String DEFAULT_EXTENSION = "dylan";
+
+  private DylanFileType() {
+    super(DylanLanguage.INSTANCE);
+  }
+
+  @NonNls
+  @NotNull
+  @Override
+  public String getName() {
+    return DylanBundle.message("dylan.file.type.name");
+  }
+
+  @NonNls
+  @NotNull
+  @Override
+  public String getDescription() {
+    return DylanBundle.message("dylan.file.type.description");
+  }
+
+  @NonNls
+  @NotNull
+  @Override
+  public String getDefaultExtension() {
+    return DEFAULT_EXTENSION;
+  }
+
+  @Override
+  public Icon getIcon() {
+    return DylanIcons.FILE;
+  }
+
+  @Override
+  public String getCharset(@NotNull VirtualFile virtualFile, byte[] bytes) {
+    return CharsetToolkit.UTF8;
+  }
+}
+
