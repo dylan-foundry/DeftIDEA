84e64d2652ea82c87169aedc1217b698295e867b
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-10-17 14:06:26 +0700

    Support method separators.

---
+++ src/org/dylanfoundry/deft/filetypes/dylan/DylanLineMarkerProvider.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013, Bruce Mitchener, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dylanfoundry.deft.filetypes.dylan;
+
+import com.intellij.codeHighlighting.Pass;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.openapi.editor.colors.CodeInsightColors;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.openapi.editor.markup.SeparatorPlacement;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.FunctionUtil;
+import org.dylanfoundry.deft.filetypes.dylan.psi.DylanDefiner;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.util.Collection;
+import java.util.List;
+
+public class DylanLineMarkerProvider implements LineMarkerProvider {
+  protected final DaemonCodeAnalyzerSettings myDaemonSettings;
+  protected final EditorColorsManager myColorsManager;
+
+  public DylanLineMarkerProvider(DaemonCodeAnalyzerSettings daemonSettings, EditorColorsManager colorsManager) {
+    myDaemonSettings = daemonSettings;
+    myColorsManager = colorsManager;
+  }
+
+  @Override
+  @Nullable
+  public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement element) {
+    if (myDaemonSettings.SHOW_METHOD_SEPARATORS) {
+      if (element instanceof DylanDefiner) {
+        LineMarkerInfo info = new LineMarkerInfo<PsiElement>(element, element.getTextRange(), null, Pass.UPDATE_ALL,
+                                                             FunctionUtil.<Object, String>nullConstant(), null,
+                                                             GutterIconRenderer.Alignment.RIGHT);
+        EditorColorsScheme scheme = myColorsManager.getGlobalScheme();
+        info.separatorColor = scheme.getColor(CodeInsightColors.METHOD_SEPARATORS_COLOR);
+        info.separatorPlacement = SeparatorPlacement.BOTTOM;
+        return info;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public void collectSlowLineMarkers(@NotNull List<PsiElement> elements, @NotNull Collection<LineMarkerInfo> result) {
+  }
+}
