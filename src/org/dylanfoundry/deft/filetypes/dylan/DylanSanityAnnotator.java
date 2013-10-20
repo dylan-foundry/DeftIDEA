83c4347187f8d898d31707d8e64971d3330e192c
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-10-20 22:28:27 +0700

    Start validating modifiers.

--- src/org/dylanfoundry/deft/filetypes/dylan/DylanSanityAnnotator.java
+++ src/org/dylanfoundry/deft/filetypes/dylan/DylanSanityAnnotator.java
@@ -22,12 +22,19 @@ import com.intellij.psi.PsiElement;
 import org.dylanfoundry.deft.filetypes.dylan.psi.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class DylanSanityAnnotator implements Annotator {
   @Override
   public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
     if (element instanceof DylanDefinitionClassDefiner) {
       DylanDefinitionClassDefiner cls = (DylanDefinitionClassDefiner) element;
       validateClass(cls, holder);
+    } else if (element instanceof  DylanDefinitionConstantDefiner) {
+      DylanDefinitionConstantDefiner constant = (DylanDefinitionConstantDefiner) element;
+      validateConstant(constant, holder);
     } else if (element instanceof DylanDefinitionFunctionDefiner) {
       DylanDefinitionFunctionDefiner function = (DylanDefinitionFunctionDefiner) element;
       validateFunction(function, holder);
@@ -44,21 +51,33 @@ public class DylanSanityAnnotator implements Annotator {
     PsiElement tail = cls.getClassDefinitionTail();
     validateDefinitionTail(tail, cls.getVariableName(), holder);
     validateClassName(cls.getVariableName(), holder);
+    validateModifiers(cls.getModifiers(), CLASS_MODIFIERS, holder);
+  }
+
+  private void validateConstant(@NotNull DylanDefinitionConstantDefiner constant, @NotNull AnnotationHolder holder) {
+    validateModifiers(constant.getModifiers(), CONSTANT_MODIFIERS, holder);
+    validateInlineModifiers(constant.getModifiers(), holder);
   }
 
   private void validateFunction(@NotNull DylanDefinitionFunctionDefiner function, @NotNull AnnotationHolder holder) {
     PsiElement tail = function.getFunctionDefinitionTail();
     validateDefinitionTail(tail, function.getVariableName(), holder);
+    validateModifiers(function.getModifiers(), FUNCTION_MODIFIERS, holder);
+    validateInlineModifiers(function.getModifiers(), holder);
   }
 
   private void validateMethod(@NotNull DylanDefinitionMethodDefiner method, @NotNull AnnotationHolder holder) {
     PsiElement tail = method.getMethodDefinitionTail();
     validateDefinitionTail(tail, method.getVariableName(), holder);
+    validateModifiers(method.getModifiers(), FUNCTION_MODIFIERS, holder);
+    validateInlineModifiers(method.getModifiers(), holder);
   }
 
   private void validateVariable(@NotNull DylanDefinitionVariableDefiner variable, @NotNull AnnotationHolder holder) {
+    validateModifiers(variable.getModifiers(), VARIABLE_MODIFIERS, holder);
     if (variable.getVariable() != null) {
-      validateVariableName(variable.getVariable().getVariableName(), holder);
+      DylanVariable var = variable.getVariable();
+      validateVariableName(var.getVariableName(), holder);
     } else {
       for (DylanVariable var : variable.getVariableList().getVariableList()) {
         validateVariableName(var.getVariableName(), holder);
@@ -90,4 +109,70 @@ public class DylanSanityAnnotator implements Annotator {
       holder.createWarningAnnotation(variableName, "Variable names usually begin with '*' and end with '*'.");
     }
   }
+
+  private void validateModifiers(DylanModifiers modifiers, Set<String> validModifiers, @NotNull AnnotationHolder holder) {
+    if ((modifiers == null) || modifiers.getModifierList() == null) {
+      return;
+    }
+    for (DylanModifier modifier : modifiers.getModifierList()) {
+      if (!validModifiers.contains(modifier.getText().trim().toLowerCase())) {
+        holder.createErrorAnnotation(modifier, "'" + modifier.getText() + "' is not a valid modifier for this definer.");
+      }
+    }
+  }
+
+  private void validateInlineModifiers(DylanModifiers modifiers, @NotNull AnnotationHolder holder) {
+    if ((modifiers == null) || modifiers.getModifierList() == null) {
+      return;
+    }
+    boolean hasInlinePolicy = false;
+    for (DylanModifier modifier : modifiers.getModifierList()) {
+      if (INLINE_POLICIES.contains(modifier.getText().trim().toLowerCase())) {
+        if (hasInlinePolicy) {
+          holder.createErrorAnnotation(modifier, "This definer already has an inline policy specified.");
+        }
+        hasInlinePolicy = true;
+      }
+    }
+  }
+
+  private final static Set<String> CLASS_MODIFIERS = new HashSet<String>(Arrays.asList(
+          "abstract",
+          "compiler-open",
+          "concrete",
+          "dynamic",
+          "free",
+          "open",
+          "primary",
+          "sealed"
+  ));
+  private final static Set<String> CONSTANT_MODIFIERS = new HashSet<String>(Arrays.asList(
+          "default-inline",
+          "inline",
+          "inline-only",
+          "may-inline",
+          "not-inline"
+  ));
+  private final static Set<String> FUNCTION_MODIFIERS = new HashSet<String>(Arrays.asList(
+          "compiler-open",
+          "compiler-sideways",
+          "default-inline",
+          "dynamic",
+          "inline",
+          "inline-only",
+          "may-inline",
+          "not-inline",
+          "open",
+          "sealed"
+  ));
+  private final static Set<String> VARIABLE_MODIFIERS = new HashSet<String>(Arrays.asList(
+          "thread"
+  ));
+  private final static Set<String> INLINE_POLICIES = new HashSet<String>(Arrays.asList(
+          "default-inline",
+          "inline",
+          "inline-only",
+          "may-inline",
+          "not-inline"
+  ));
 }
