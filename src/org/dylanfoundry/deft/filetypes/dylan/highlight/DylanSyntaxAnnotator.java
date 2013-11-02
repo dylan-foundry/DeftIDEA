1b3ff50297955d4bde50fe68711c40de23d052e4
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-11-02 18:13:42 +0700

    Highlight definer type names.

--- src/org/dylanfoundry/deft/filetypes/dylan/highlight/DylanSyntaxAnnotator.java
+++ src/org/dylanfoundry/deft/filetypes/dylan/highlight/DylanSyntaxAnnotator.java
@@ -16,10 +16,12 @@
 
 package org.dylanfoundry.deft.filetypes.dylan.highlight;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
 import org.dylanfoundry.deft.filetypes.dylan.highlight.DylanSyntaxHighlighterColors;
 import org.dylanfoundry.deft.filetypes.dylan.psi.*;
 import org.jetbrains.annotations.NotNull;
@@ -41,24 +43,42 @@ public class DylanSyntaxAnnotator implements Annotator {
     } else if (element instanceof DylanDefinitionClassDefiner) {
       DylanDefinitionClassDefiner cls = (DylanDefinitionClassDefiner) element;
       highlightModifiers(cls.getModifiers(), holder);
+      highlightDefinerType(cls, DylanTypes.CLASS, holder);
+      highlightDefinerType(cls.getClassDefinitionTail(), DylanTypes.CLASS, holder);
     } else if (element instanceof DylanDefinitionConstantDefiner) {
       DylanDefinitionConstantDefiner constant = (DylanDefinitionConstantDefiner) element;
       highlightModifiers(constant.getModifiers(), holder);
+      highlightDefinerType(constant, DylanTypes.CONSTANT_T, holder);
     } else if (element instanceof DylanDefinitionDomainDefiner) {
       DylanDefinitionDomainDefiner domain = (DylanDefinitionDomainDefiner) element;
       highlightModifiers(domain.getModifiers(), holder);
+      highlightDefinerType(domain, DylanTypes.DOMAIN, holder);
     } else if (element instanceof DylanDefinitionFunctionDefiner) {
       DylanDefinitionFunctionDefiner function = (DylanDefinitionFunctionDefiner) element;
       highlightModifiers(function.getModifiers(), holder);
+      highlightDefinerType(function, DylanTypes.FUNCTION, holder);
+      highlightDefinerType(function.getFunctionDefinitionTail(), DylanTypes.FUNCTION, holder);
     } else if (element instanceof DylanDefinitionGenericDefiner) {
       DylanDefinitionGenericDefiner generic = (DylanDefinitionGenericDefiner) element;
       highlightModifiers(generic.getModifiers(), holder);
+      highlightDefinerType(generic, DylanTypes.GENERIC, holder);
     } else if (element instanceof DylanDefinitionMethodDefiner) {
       DylanDefinitionMethodDefiner method = (DylanDefinitionMethodDefiner) element;
       highlightModifiers(method.getModifiers(), holder);
+      highlightDefinerType(method, DylanTypes.METHOD, holder);
+      highlightDefinerType(method.getMethodDefinitionTail(), DylanTypes.METHOD, holder);
+    } else if (element instanceof DylanDefinitionSuiteDefiner) {
+      DylanDefinitionSuiteDefiner suite = (DylanDefinitionSuiteDefiner) element;
+      highlightDefinerType(suite, DylanTypes.SUITE, holder);
+      highlightDefinerType(suite.getSuiteDefinitionTail(), DylanTypes.SUITE, holder);
+    } else if (element instanceof DylanDefinitionTestDefiner) {
+      DylanDefinitionTestDefiner test = (DylanDefinitionTestDefiner) element;
+      highlightDefinerType(test, DylanTypes.TEST, holder);
+      highlightDefinerType(test.getTestDefinitionTail(), DylanTypes.TEST, holder);
     } else if (element instanceof DylanDefinitionVariableDefiner) {
       DylanDefinitionVariableDefiner variable = (DylanDefinitionVariableDefiner) element;
       highlightModifiers(variable.getModifiers(), holder);
+      highlightDefinerType(variable, DylanTypes.VARIABLE_T, holder);
     }
   }
 
@@ -66,6 +86,14 @@ public class DylanSyntaxAnnotator implements Annotator {
     if (modifiers != null) {
       Annotation annotation = holder.createInfoAnnotation(modifiers, null);
       annotation.setTextAttributes(DylanSyntaxHighlighterColors.MODIFIERS);
+    }
+  }
+
+  private static final void highlightDefinerType(PsiElement element, IElementType type, @NotNull AnnotationHolder holder) {
+    ASTNode node = element.getNode().findChildByType(type);
+    if (node != null) {
+      Annotation annotation = holder.createInfoAnnotation(node, null);
+      annotation.setTextAttributes(DylanSyntaxHighlighterColors.KEYWORD);
     }
   }
 }
